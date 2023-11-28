ec1095c9b2351214197d21a76fe55056356caf6f
Cyber-420 <152280025+Cyber-420@users.noreply.github.com>
2023-11-28 18:40:15 +0600

    Add files via upload

---
+++ termux-setup.py
@@ -0,0 +1,100 @@
+import os,time
+from time import sleep as time
+from os import system as rafsan
+print("\033[1;31mjoin our Facebook group")
+time(2)
+rafsan("termux-open https://www.facebook.com/groups/540136494232116/?ref=share&mibextid=NSMWBT")
+os.system('clear')
+logo = (f""" 
+  ______             __                           
+ /      \           /  |                          
+/$$$$$$  | __    __ $$ |____    ______    ______  
+$$ |  $$/ /  |  /  |$$      \  /      \  /      \ 
+$$ |      $$ |  $$ |$$$$$$$  |/$$$$$$  |/$$$$$$  |
+$$ |   __ $$ |  $$ |$$ |  $$ |$$    $$ |$$ |  $$/ 
+$$ \__/  |$$ \__$$ |$$ |__$$ |$$$$$$$$/ $$ |      
+$$    $$/ $$    $$ |$$    $$/ $$       |$$ |      
+ $$$$$$/   $$$$$$$ |$$$$$$$/   $$$$$$$/ $$/       
+          /  \__$$ |                              
+          $$    $$/                               
+           $$$$$$/                                
+""") 
+print(logo)
+
+
+def packege():
+	rafsan("pkg update -y")
+	rafsan("pkg upgrade -y")
+	rafsan("pkg install python -y")
+	rafsan("pkg install python2 -y")
+	rafsan("pkg install python2-dev -y")
+	rafsan("pkg install python3 -y")
+	rafsan("pip install requests")
+	rafsan("pip2 install requests")
+	rafsan("pip install mechanize")
+	rafsan("pip2 install mechanize")
+	rafsan("pip install lolcat")
+	rafsan("pip install bs4")
+	rafsan("pip2 install bs4")
+	rafsan("pip2 install requests bs4")
+	rafsan("pip install futures")
+	rafsan("pip2 install futures")
+	rafsan("pip install rich")
+	rafsan("pkg install java -y")
+	rafsan("pkg install fish -y")
+	rafsan("pkg install ruby -y")
+	rafsan("pkg install help -y")
+	rafsan("pkg install git -y")
+	rafsan("pkg install host -y")
+	rafsan("pkg install php -y")
+	rafsan("pkg install perl -y")
+	rafsan("pkg install nmap -y")
+	rafsan("pkg install bash -y")
+	rafsan("pkg install clang -y")
+	rafsan("pkg install nano -y")
+	rafsan("pkg install w3m -y")
+	rafsan("pkg install havij -y")
+	rafsan("pkg install hydra -y")
+	rafsan("pkg install figlet -y")
+	rafsan("pkg install cowsay -y")
+	rafsan("pkg install curl -y")
+	rafsan("pkg install tar -y")
+	rafsan("pkg install zip -y")
+	rafsan("pkg install unzip -y")
+	rafsan("pkg install tor -y")
+	rafsan("pkg install google -y")
+	rafsan("pkg install sudo -y")
+	rafsan("pkg install wget -y")
+	rafsan("pkg install wireshark -y")
+	rafsan("pkg install wgetrc -y")
+	rafsan("pkg install wcalc -y")
+	rafsan("pkg install bmon -y")
+	rafsan("pkg install vpn -y")
+	rafsan("pkg install unrar -y")
+	rafsan("pkg install toilet -y")
+	rafsan("pkg install proot -y")
+	rafsan("pkg install net-tools -y")
+	rafsan("pkg install golang -y")
+	rafsan("pkg install chroot -y")
+	rafsan("termux-chroot -y")
+	rafsan("pkg install macchanger-y")
+	rafsan("pkg install openssl -y")
+	rafsan("pkg install cmatrix -y")
+	rafsan("pkg install openssh -y")
+	rafsan("pkg install wireshark -y")
+	rafsan("termux-setup-storage -y")
+	rafsan("pkg install macchanger -y")
+	rafsan("apt update && apt upgrade -y")
+	rafsan("pip install requirements")
+	rafsan("pkg install espeak")
+	rafsan("pip install espeak")
+	rafsan("pip install ")
+	rafsan("pip install --upgrade ")
+print("[A] Start Besic setup")
+print("[B] Contract Admin")
+print("[C] Exit")
+fuck=input("[??]Choice Menu:")
+if fuck in["A"]:
+	packege()
+elif fuck in["B"]:
+	rafsan("termux-open https://www.facebook.com/hamim.bhuyan.31://www.facebook.com/groups/540136494232116/?ref=share&mibextid=NSMWBT")
